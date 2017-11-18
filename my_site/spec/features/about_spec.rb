26ef0a9f108d7c6e3ea4c7184902e5cddb677a19
Francesco <francesco.falchi@gmail.com>
2017-11-19 00:47:09 +0100

    Make image in About page width 100%

---
+++ my_site/spec/features/about_spec.rb
@@ -0,0 +1,24 @@
+# describe 'about' do
+#
+# end
+#
+# it 'shows a link to contact' do
+#   expect(page).to have_css 'a', text: 'Contact'
+# end
+# require 'spec_helper'
+#
+# describe 'about', type: :feature do
+#
+#   before do
+#     visit '/'
+#   end
+#
+#   it 'displays CV list' do
+#      expect(page).to have_css '.CV'
+#      within '.CV' do
+#        expect(page).to have_content 'HYPER ISLAND'
+#        expect(page).to have_content 'Florence, IT'
+#        expect(page).to have_content 'Digital Media Programme'
+#      end
+#   end
+# end
--- my_site/spec/features/index_spec.rb
+++ my_site/spec/features/index_spec.rb
@@ -1,3 +1,5 @@
+
+
 it 'displays project list' do
   expect(page).to have_css '.projects'
   within '.projects' do
